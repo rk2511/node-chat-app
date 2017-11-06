3d130b12e1640fa86ff72b957282cd41f91bd348
vikki <radhakrishnan1125@gmail.com>
2017-11-07 01:22:58 +0530

    added socket io

--- server/server.js
+++ server/server.js
@@ -1,10 +1,28 @@
 const path = require('path');
 const express = require('express');
+//const http = require('http');
+//const socketIO = require('socket.io');
+//var app = express();
+//var server = http.createServer(app);
+//var io = socketIO(server);
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 const port = process.env.PORT || 3000;
 var publicPath = path.join(__dirname,'../public');
-var app = express();
+
 app.use(express.static(publicPath));
 console.log(publicPath);
-app.listen(port, () => {
+
+io.on('connection', (socket) => {
+  console.log('New user connected');
+
+  socket.on('disconnect', ()=> {
+    console.log('user disconnected');
+  });
+});
+
+
+http.listen(port, () => {
   console.log('server up on port 3000');
 });
