662349306c4373162aad48aea68700c6fbe5dde8
vikki <radhakrishnan1125@gmail.com>
2017-11-12 21:06:39 +0530

    added chat home page

---
+++ public/js/chat.js
@@ -0,0 +1,90 @@
+var socket = io();
+
+function scrollToBottom () {
+  //selectors
+  var messages = jQuery('#messages');
+  var newMessage = messages.children('li:last-child');
+  //Heights
+  var clientHeight = messages.prop('clientHeight');
+  var scrollTop = messages.prop('scrollTop');
+  var scrollHeight = messages.prop('scrollHeight');
+  var newMessageHeight = newMessage.innerHeight();
+  var lastMessageHeight = newMessage.prev().innerHeight();
+
+  if(clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+    messages.scrollTop(scrollHeight);
+  }
+}
+socket.on('connect', () =>{
+  console.log('connected to server');
+});
+
+socket.on('disconnect', () => {
+  console.log('disconnected from server');
+});
+
+socket.on('newMessage', (message) => {
+var formattedTime = moment(message.createdAt).format('h:mm a');
+// console.log('got new message', message);
+// var li = jQuery('<li></li>');
+// li.text(`${message.from}, ${formattedTime}: ${message.text}`);
+// jQuery('#messages').append(li);
+var template = jQuery('#message-template').html();
+var html = Mustache.render(template, {
+  text: message.text,
+  from: message.from,
+  createdAt : formattedTime
+});
+jQuery('#messages').append(html);
+scrollToBottom();
+});
+
+socket.on('newLocationMessage', (message) => {
+var formattedTime = moment(message.createdAt).format('h:mm a');
+// var li = jQuery('<li></li>');
+// var a = jQuery('<a target ="_blank">My current Location</a>');
+// li.text(`${message.from}, ${formattedTime}: `);
+// a.attr('href',message.url);
+// li.append(a);
+// jQuery('#messages').append(li);
+var template = jQuery('#location-message-template').html();
+var html = Mustache.render(template, {
+  url: message.url,
+  from: message.from,
+  createdAt : formattedTime
+});
+jQuery('#messages').append(html);
+scrollToBottom();
+});
+
+jQuery('#message-form').on('submit', function(e) {
+  e.preventDefault();
+  var messageTextBox = jQuery('[name=message]');
+  socket.emit('createMessage', {
+    from: 'User',
+    text: messageTextBox.val()
+  }, function () {
+    messageTextBox.val('');
+  });
+});
+
+var locationButton = jQuery('#send-location');
+locationButton.on('click', function() {
+  if(!navigator.geolocation) {
+    return alert('Geolocator not supported by the brower');
+  }
+  locationButton.attr('disabled', 'disabled');
+  locationButton.text('Sending...');
+  navigator.geolocation.getCurrentPosition(function (position) {
+  locationButton.removeAttr('disabled');
+  locationButton.text('Send Location');
+    socket.emit('createLocateMessage', {
+      latitude: position.coords.latitude,
+      longitude: position.coords.longitude
+    })
+  }, function () {
+    locationButton.removeAttr('disabled');
+    alert('Unable to get location');
+
+  });
+});
